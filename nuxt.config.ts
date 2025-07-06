c3b756c28783457e5388f9f1e6d46f078ea5d53d
Felipe Evaldt Carvalho <felipeevaldt1@gmail.com>
2025-07-06 20:33:05 -0300

    Adiciona configuração do Tailwind CSS, atualiza estrutura do app e configurações do ESLint. Inclui novos arquivos de configuração e estilos, além de ajustes na configuração do Nuxt.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: process.env.NODE_ENV === "development" },
   modules: [
-    '@nuxt/image',
-    '@nuxt/icon',
-    '@nuxt/fonts',
-    '@nuxt/eslint',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@nuxt/eslint",
+    "@nuxt/scripts",
+    "@nuxt/test-utils",
+    "@nuxt/ui",
+  ],
+  css: ["~/assets/css/main.css"],
+  ui: {
+    colorMode: false,
+  },
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL,
+    },
+  },
+});
