e3ef877024478af47c0d3eec0d4c3abf3c41047e
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2025-07-11 18:47:42 -0300

    feat: Adiciona middleware de redirecionamento para a página de login

--- pages/auth.vue
+++ pages/auth.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["to-login"],
+});
+</script>
 <template>
   <div
     class="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8"
@@ -5,5 +10,3 @@
     <NuxtPage />
   </div>
 </template>
-
-<script setup lang="ts"></script>
